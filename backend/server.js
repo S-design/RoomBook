2762689d6cb1e2908d753386125c02dfc42cb474
S-design <e.sisley@icloud.com>
2025-01-28 12:57:38 +0000

    Config

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 //for configuration
 const PORT = process.env.PORT || 5000;
 const PIN = process.env.PIN || '2334';
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'https://s-design.github.io/RoomBook';
 
 const hashedPin = bcrypt.hashSync(PIN, 10);
 
@@ -23,8 +23,9 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: FRONTEND_URL, // Replace with your frontend's URL during development
-    methods: ['POST'], // Allow POST requests
+    origin: [FRONTEND_URL], // Replace with your frontend's URL during development
+    methods: ['POST'],
+    credentials: true, // Allow POST requests
 }));
 app.use(helmet());
 
